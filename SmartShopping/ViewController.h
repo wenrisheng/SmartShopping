d98f3a0250965d7c33cbd6a052b89be54ec8803f
wrs <252797991@qq.com>
2015-04-07 21:52:48 +0800

    Initial Commit

---
+++ SmartShopping/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartShopping
+//
+//  Created by wrs on 15/4/7.
+//  Copyright (c) 2015年 wrs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmartShopping/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartShopping
+//
+//  Created by wrs on 15/4/7.
+//  Copyright (c) 2015年 wrs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
